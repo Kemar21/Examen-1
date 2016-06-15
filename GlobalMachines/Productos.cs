7c6448bf7b9976cd9146b161e5f8298f043fe6c2
kemar21 <kembly.arias.21@gmail.com>
2016-06-15 17:46:13 -0600

    examen uno Please enter the commit message for your changes. Lines starting

---
+++ GlobalMachines/Productos.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalMachines
+{
+    class Productos
+    {
+        private int codigo_articulo;
+        private string nombre_articulo;
+        private string tipo;
+        private int fecha_vencimiento;
+        private string nombre_proveedor;
+        private int posicion_estante;
+        private int precio;
+        public double tamanno;
+
+        private struct Productos
+        {
+            public int codigo_articulo;
+            public string nombre_articulo;
+            public string tipo;
+            public int fecha_vencimiento;
+            public string nombre_proveedor;
+            public int posion_estante;
+            public int precio;
+
+        };
+
+        private static Productos[] producto;
+        
+        
+        public Productos() {
+
+            codigo_articulo = 0;
+            nombre_articulo = "";
+            tipo = "";
+            fecha_vencimiento = 0;
+            posicion_estante = 0;
+            precio = 0;
+
+           }
+
+        public Productos(int codigo_articulo, string nombre_articulo, string tipo, int fecha_vencimiento,string nombre_proveedor, int posicion_estante, int precio)
+        {
+            this.codigo_articulo = codigo_articulo;
+            this.nombre_articulo = nombre_articulo;
+            this.tipo = tipo;
+            this.fecha_vencimiento = fecha_vencimiento;
+            this.nombre_proveedor = nombre_proveedor;
+            this.posicion_estante = posicion_estante;
+            this.precio = precio;
+        }
+
+        public void setCodigoArticulo(int codigo_articulo)
+        {
+            this.codigo_articulo = codigo_articulo;
+        }
+
+        public int getCodigoArticulo()
+        {
+            return codigo_articulo;
+
+        }
+
+
+        public void setNombreArticulo(string nombre_articulo)
+        {
+            this.nombre_articulo = nombre_articulo;
+        }
+
+        public string getNombreArticulo()
+        {
+            return nombre_articulo;
+        }
+
+        public void setTipo(string tipo)
+        {
+            this.tipo = tipo;
+        }
+
+        public string getTipo()
+        {
+            return tipo;
+        }
+
+        public void setFechaVencimiento(int fecha_vencimiento)
+        {
+            this.fecha_vencimiento = fecha_vencimiento;
+        }
+
+        public int getFechaVencimiento()
+        {
+            return fecha_vencimiento;
+        }
+
+        public void setNombreProveedor(string nombre_proveedor)
+        {
+            this.nombre_proveedor = nombre_proveedor;
+        }
+
+        public string getNombreProveedor()
+        {
+            return nombre_proveedor;
+        }
+
+        public void setPosicionEstante(int posicion_estante)
+        {
+            this.posicion_estante=posicion_estante;
+        }
+
+        public int getPosicionEstante()
+        {
+            return posicion_estante;
+        }
+
+        public void setPrecio(int precio)
+        {
+            this.precio = precio;
+        }
+
+        public int getPrecio()
+        {
+            return precio;
+        }
+
+        public void setTamanno(int Tamanno)
+        {
+            tamanno = Tamanno;
+            producto = new Productos[Tamanno];
+        }
+
+       
+
+        //public void Registro ()
+        //{
+        //    for(int i = 0; i < producto.Length; i++){
+
+        //        producto[i].codigo_articulo = 0;
+        //        producto[i].nombre_articulo = " ";
+        //        producto[i].tipo = "";
+        //        producto[i].fecha_vencimiento = 0;
+        //        producto[i].nombre_proveedor = "";
+        //        producto[i].posion_estante = 0;
+        //        producto[i].precio = 0;
+
+        //    }
+
+        //}
+      
+       
+        public bool ingresoProducto(Productos Producto)
+            {
+
+            int i=0;
+                for (i = 0; i < producto.Length; i++)
+                {
+                    if (producto[i].codigo_articulo == 0)
+                    {
+                        break;
+                    }
+                }
+
+                if (i < producto.Length)
+                {
+                    producto[i].codigo_articulo= codigo_articulo;
+                    producto[i].nombre_articulo = nombre_articulo;
+                    producto[i].tipo = tipo;
+                    producto[i].fecha_vencimiento=fecha_vencimiento;
+                    producto[i].nombre_proveedor = nombre_proveedor;
+                    producto[i].posion_estante = posicion_estante;
+                    producto[i].precio = precio;
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+        //metodo par buscar los articulos 
+          public bool buscarArticulo(int record)
+            {
+                int i;
+                for (i = 0; i <  producto.Length; i++)
+                {
+                    if (producto[i].codigo_articulo == record)
+                    {
+                        break;
+                    }
+                }
+
+                if (i < producto.Length)
+                {
+                    producto[i].codigo_articulo= codigo_articulo;
+                    producto[i].nombre_articulo = nombre_articulo;
+                    producto[i].tipo = tipo;
+                    producto[i].fecha_vencimiento=fecha_vencimiento;
+                    producto[i].nombre_proveedor = nombre_proveedor;
+                    producto[i].posion_estante = posicion_estante;
+                    producto[i].precio = precio;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+        public bool modificarArticulo(int articulo)
+            {
+                int i;
+                for (i = 0; i <  producto.Length; i++)
+                {
+                    if (producto[i].codigo_articulo == articulo)
+                    {
+                        break;
+                    }
+                }
+
+                if (i < producto.Length)
+                {
+                    producto[i].codigo_articulo= codigo_articulo;
+                    producto[i].nombre_articulo = nombre_articulo;
+                    producto[i].tipo = tipo;
+                    producto[i].fecha_vencimiento=fecha_vencimiento;
+                    producto[i].nombre_proveedor = nombre_proveedor;
+                    producto[i].posion_estante = posicion_estante;
+                    producto[i].precio = precio;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        public bool borrarArticulo(int articulo)
+            {
+                int i;
+                for (i = 0; i <  producto.Length; i++)
+                {
+                    if (producto[i].codigo_articulo == articulo)
+                    {
+                        break;
+                    }
+                }
+
+                if (i < producto.Length)
+                {
+                    producto[i].codigo_articulo= codigo_articulo;
+                    producto[i].nombre_articulo = nombre_articulo;
+                    producto[i].tipo = tipo;
+                    producto[i].fecha_vencimiento=fecha_vencimiento;
+                    producto[i].nombre_proveedor = nombre_proveedor;
+                    producto[i].posion_estante = posicion_estante;
+                    producto[i].precio = precio;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+        public void imprimirRegistro(int print)
+        {
+            codigo_articulo = producto[print].codigo_articulo;
+            nombre_articulo =producto[print].nombre_articulo;
+            tipo = producto[print].tipo;
+            fecha_vencimiento = producto[print].fecha_vencimiento;
+            nombre_proveedor = producto[print].nombre_proveedor;
+            posicion_estante = producto[print].posion_estante;
+            precio = producto[print].precio;
+
+        }
+        ~Productos()
+        {
+
+        }
+    }
+}
---
+++ GlobalMachines/Program.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalMachines
+{
+    class Program
+    {
+
+        static Productos producto = new Productos();
+
+        static int opcion = 0;
+        static int tamanno = 6;
+        [Flags]
+        enum ProductosNivel
+        {
+            nivelAlto = 1,
+            nivelMedioAlto = 2,
+            nivelMedio = 3,
+            nivelMedioBajo = 4,
+            nivelBajo = 5,
+        };
+
+
+        static void ingresoProducto()
+        {
+
+            int i = 0;
+            int j = 0;
+            while (i != 0)
+            {
+                Console.Clear();
+                if (i < producto.tamanno)
+                {
+                    Console.WriteLine("Ingreso de productos...");
+                    Console.WriteLine("Ingrese el codigo del procuto");
+                    producto.setCodigoArticulo(int.Parse(Console.ReadLine()));
+                    Console.WriteLine("Ingrese el nombre del producto");
+                    producto.setNombreArticulo(Console.ReadLine());
+                    Console.WriteLine("Ingrese el tipo del producto");
+                    producto.setTipo(Console.ReadLine());
+                    Console.WriteLine("Ingrese la fecha de vencimiento del producto");
+                    producto.setFechaVencimiento(int.Parse(Console.ReadLine()));
+                    Console.WriteLine("Ingrese el proveedor del producto");
+                    producto.setNombreProveedor(Console.ReadLine());
+                    Console.WriteLine("Ingrese la posicion del producto");
+                    Console.WriteLine(ProductosNivel.nivelAlto + "    " + (int)ProductosNivel.nivelAlto + "\n"
+                    + ProductosNivel.nivelMedioAlto + "   " + (int)ProductosNivel.nivelMedioAlto + "\n" +
+                    ProductosNivel.nivelMedio + "   " + (int)ProductosNivel.nivelMedio + "\n" +
+                    ProductosNivel.nivelMedioBajo + "" + (int)ProductosNivel.nivelMedioBajo + "\n" +
+                    ProductosNivel.nivelBajo + "" + (int)ProductosNivel.nivelBajo + "\n");
+                    producto.setPosicionEstante(int.Parse(Console.ReadLine()));
+                    if (producto.ingresoProducto(producto) == true)
+                    {
+                        Console.WriteLine("El producto ha sido agregado");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El producto no se ha agregado correctamente ");
+                        i = int.Parse(Console.ReadLine());
+                        j++;
+                        Console.ReadKey();
+                    }
+                }
+
+
+            }
+
+        }
+        static void modificarProducto()
+        {
+            int reg = 0;
+            int r = 1;
+            while (r != 1)
+            {
+                Console.Clear();
+                Console.WriteLine("Digite el ID del libro que desea modificar");
+                int codigoProducto = int.Parse(Console.ReadLine());
+                while (r != 2)
+                {
+
+
+                    if (producto.buscarArticulo(codigoProducto) == true)
+                    {
+                        Console.WriteLine("Digite el nuevo ID del libro");
+                        int newCodigo = int.Parse(Console.ReadLine());
+
+                        reg = codigoProducto;
+                        Console.WriteLine("Registro: " + reg);
+                        Console.WriteLine("Codigo:       " + newCodigo);
+                        Console.WriteLine("Nombre:     " + producto.getNombreArticulo());
+                        Console.WriteLine("Tipo:     " + producto.getTipo());
+                        Console.WriteLine("Fecha de Vencimiento     " + producto.getFechaVencimiento());
+                        Console.WriteLine("Nombre del proveedor     " + producto.getNombreProveedor());
+                        Console.WriteLine("Posicion estante     " + producto.getPosicionEstante());
+                        Console.WriteLine("Precio:   " + producto.getPrecio());
+                        Console.WriteLine("Desea buscar otro libro (1)Sí (2)No");
+                        r = 2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("No se encontro ese ID");
+                        Console.WriteLine("Desea buscar otro libro 0-Sí 1-No");
+                        r = int.Parse(Console.ReadLine());
+                    }
+                }
+            } Console.ReadKey();
+        }
+
+        static void eliminarProducto()
+        {
+            int reg = 0;
+             int r = 1;
+            while (r != 1)
+            {
+                Console.Clear();
+                Console.WriteLine("-* Librería el Buen Lector  *-*");
+                Console.WriteLine("Modificacion de libros.           ");
+                Console.WriteLine("Digite el ID del libro que desea eliminar");
+                int codigoProducto= int.Parse(Console.ReadLine());
+                while (r != 2)
+                {
+                    if (producto.borrarArticulo(codigoProducto)== true)
+                    {
+                        codigoProducto = 0;
+                        //reg = codigoProducto;
+                        Console.WriteLine("Registro: " + reg);
+                        Console.WriteLine("Codigo:       " + codigoProducto);
+                        Console.WriteLine("Nombre:     " + producto.getNombreArticulo());
+                        Console.WriteLine("Tipo:     " + producto.getTipo());
+                        Console.WriteLine("Fecha de Vencimiento     " + producto.getFechaVencimiento());
+                        Console.WriteLine("Nombre del proveedor     " + producto.getNombreProveedor());
+                        Console.WriteLine("Posicion estante     " + producto.getPosicionEstante());
+                        Console.WriteLine("Precio:   " + producto.getPrecio());
+                    
+                        Console.WriteLine("Desea buscar otro libro 0-Sí 1-No");
+                        r = 2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("No se encontro ese ID");
+                        Console.WriteLine("Desea buscar otro libro 0-Sí 1-No");
+                        r = int.Parse(Console.ReadLine());
+                    }
+                }
+            } Console.ReadKey();
+        }
+
+      
+
+        static void buscarProducto()
+        {
+
+            int reg = 0; 
+            int  i = 0;
+            while (i != 1)
+            {
+                Console.Clear();
+              
+                Console.WriteLine("Ingrese el codigo del producto");
+                int codigoProducto = int.Parse(Console.ReadLine());
+                while (i != 2)
+                {
+                    if (producto.buscarArticulo(codigoProducto) == true)
+                    {
+                        reg = codigoProducto;
+                        Console.WriteLine("Registro: " + reg);
+                        Console.WriteLine("Codigo del procuto:     " + producto.getCodigoArticulo());
+                        Console.WriteLine("Nombre:     " + producto.getNombreArticulo());
+                        Console.WriteLine("Tipo:     " + producto.getTipo());
+                        Console.WriteLine("Fecha de Vencimiento     " + producto.getFechaVencimiento());
+                        Console.WriteLine("Nombre del proveedor     " + producto.getNombreProveedor());
+                        Console.WriteLine("Posicion estante     " + producto.getPosicionEstante());
+                        Console.WriteLine("Precio:   " + producto.getPrecio());
+
+                        i = 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("No se encontro ese codigo");
+                        Console.WriteLine("Desea buscar otro libro \n 1(Sí) 2(No)");
+                        i = int.Parse(Console.ReadLine());
+                    }
+                }
+            } Console.ReadKey();
+        }
+
+        static void listadoProductos()
+        {
+            int reg = 0;
+            int i;
+            Console.Clear();
+
+            Console.WriteLine("Listado de libros");
+            for (i = 0; i < producto.tamanno; i++)
+            {
+                producto.imprimirRegistro(i);
+                reg = i + 1;
+                Console.WriteLine("Reg: " + reg);
+                Console.WriteLine("Codigo del procuto:     " + producto.getCodigoArticulo());
+                Console.WriteLine("Nombre:     " + producto.getNombreArticulo());
+                Console.WriteLine("Tipo:     " + producto.getTipo());
+                Console.WriteLine("Fecha de Vencimiento     " + producto.getFechaVencimiento());
+                Console.WriteLine("Nombre del proveedor     " + producto.getNombreProveedor());
+                Console.WriteLine("Posicion estante     " + producto.getPosicionEstante());
+                Console.WriteLine("Precio:   " + producto.getPrecio());
+            }
+            Console.ReadKey();
+        }
+        static void Main()
+        {
+            //inicializar el objeto
+            producto.setPrecio(tamanno);
+            //producto.Registro();
+
+            // PrintInvoice();
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("-* Librería el Buen Lector  *-*");
+                Console.WriteLine("1. Ingreso de libros.");
+                Console.WriteLine("2. Modificación de libros.");
+                Console.WriteLine("3. Eliminación de libros.");
+                Console.WriteLine("4. Búsquedad de libros.");
+                Console.WriteLine("5. Listado de libros.");
+                Console.WriteLine("6. Venta de libros.");
+                Console.WriteLine("7. Salir.");
+                Console.WriteLine("Selecciones una opción");
+                opcion = int.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        {
+                            ingresoProducto();
+                            break;
+                        }
+                    case 2:
+                        {
+                            modificarProducto();
+                            //Modificar la información
+                            break;
+                        }
+                    case 3:
+                        {
+                            eliminarProducto();
+                            //Eliminar información dejando campos en 0 o vacío
+                            break;
+                        }
+                    case 4:
+                        {
+                            buscarProducto();
+                            break;
+                        }
+                    case 5:
+                        {
+                            listadoProductos();
+                            break;
+                        }
+                    case 6:
+                        {
+                            //Venta similar a tarea1
+                            break;
+                        }
+                    case 7:
+                        
+                        break;
+                    default:
+                        {
+                            Console.WriteLine("Seleccione inválida..");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+                        }
+                }
+            } while (opcion != 7);
+
+            Console.ReadKey();
+        }
+
+        
+    }
+}
+
+        
+
+
+
+
+
+
+
+
+
+
+
+//        static void Main(string[] args)
+//        {
+
+//        }
+//    }
+//}
